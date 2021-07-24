90e00e7be782a2f2b4f70a1612ffa7c88df153c5
mr.huang <max666688@126.com>
2021-07-24 10:43:37 +0800

    Initial commit

---
+++ PrepareEaxm/PointTest/PointDefine.cpp
@@ -0,0 +1,14 @@
+//
+// Created by max66 on 2021/7/24.
+//
+
+#include <iostream>
+#include "PointDefine.h"
+
+
+int main(){
+
+    cout<< "hello world!" << endl;
+
+    return 0;
+}
---
+++ PrepareEaxm/PointTest/PointDefine.h
@@ -0,0 +1,14 @@
+//
+// Created by max66 on 2021/7/24.
+//
+
+#ifndef MY_CPLUS_POINTDEFINE_H
+#define MY_CPLUS_POINTDEFINE_H
+using namespace std;
+
+class PointDefine {
+
+};
+
+
+#endif //MY_CPLUS_POINTDEFINE_H
