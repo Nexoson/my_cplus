3f9e038b5c4b1443b4f9a35ad5b5e62eb20801a1
mr.huang <max666688@126.com>
2021-07-25 15:08:24 +0800

    feat: 结构体与数组

---
+++ prepare_eaxm/structure_test/structure_arr/StructureArr.cpp
@@ -0,0 +1,28 @@
+//
+// Created by max66 on 2021/7/25.
+//
+
+#include "StructureArr.h"
+
+// 结构体定义
+struct student{
+    // 成员列表
+    string name; // 姓名
+    int age;     // 年龄
+    int score;   // 分数
+};
+
+int main(){
+    // 结构体数组
+    struct student arr[3]={
+            {"张三",18,80 },
+            {"李四",19,60 },
+            {"王五",20,70 }
+    };
+
+    for (int i = 0; i < 3; ++i) {
+        cout << "姓名：" << arr[i].name << " 年龄：" << arr[i].age << " 分数：" << arr[i].score << endl;
+    }
+
+    return 0;
+}
---
+++ prepare_eaxm/structure_test/structure_arr/StructureArr.h
@@ -0,0 +1,15 @@
+//
+// Created by max66 on 2021/7/25.
+//
+
+#ifndef MY_CPLUS_STRUCTUREARR_H
+#define MY_CPLUS_STRUCTUREARR_H
+#include <iostream>
+using namespace std;
+
+class StructureArr {
+
+};
+
+
+#endif //MY_CPLUS_STRUCTUREARR_H
