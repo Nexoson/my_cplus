c8d043aa98590798cbb3e894ba6abbe6add1240d
mr.huang <max666688@126.com>
2021-07-25 19:42:57 +0800

    feat: 结构体案例

---
+++ prepare_eaxm/structure_test/structure_demo/StructureDemo.h
@@ -0,0 +1,16 @@
+//
+// Created by max66 on 2021/7/25.
+//
+
+#ifndef MY_CPLUS_STRUCTUREDEMO_H
+#define MY_CPLUS_STRUCTUREDEMO_H
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+class StructureDemo {
+
+};
+
+
+#endif //MY_CPLUS_STRUCTUREDEMO_H
